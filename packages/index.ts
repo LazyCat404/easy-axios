9ce93f933703088f1ea6772f3e28efaa41e3346f
yuwenjie <wenjie_address@outlook.com>
2025-07-11 10:41:04 +0800

    v1.0.0

---
+++ packages/index.ts
@@ -0,0 +1,12 @@
+import easyAxios from './easyAxiosConfig';
+import get from './get';
+import post from './post';
+import put from './put';
+import del from './del';
+import Loading from './Loading';
+
+export { get, post, put, del, Loading };
+
+export type { easyAxiosOptions } from './easyAxiosConfig';
+
+export default easyAxios;
---
+++ test/main.ts
@@ -0,0 +1,13 @@
+import easyAxios, { post } from '../packages/index';
+
+easyAxios({
+  baseURL: 'http://localhost:3000'
+});
+
+const api = {
+  login: (par: any): Promise<any> => post('/user/login', par)
+};
+
+api.login({ username: 'test', password: '123456' }).then(res => {
+  console.log('Login Response:', res);
+});
